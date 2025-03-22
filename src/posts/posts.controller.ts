8c588401385071b31b51086b1272700a15207251
John <lamagniguitoure@gmail.com>
2025-03-22 14:53:40 +0000

    Add postman file

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { CreatePostDto } from "./dtos/createPost.dto";
 import { PostsService } from "./posts.service";
+import { UpdatePostDto } from "./dtos/update-post.dto";
 
 
 @Controller('posts')
@@ -11,4 +12,26 @@ export class PostsController {
     createPost(@Body() createPostDto: CreatePostDto) {
         return this.postsService.createPost(createPostDto);
     }
+
+    @Get()
+    getPosts() {
+        return this.postsService.getPosts();
+    }
+
+    @Get(':id')
+    getPostById(@Param('id') id: string) {
+        return this.postsService.getPostById(id);
+    }
+
+    @Patch(':id')
+    updatePost(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+        return this.postsService.updatePost(id, updatePostDto);
+    }
+
+    @Delete(':id')
+    deletePost(@Param('id') id: string) {
+        return this.postsService.deletePost(id);
+    }
+
+
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,9 +1,10 @@
 import { HttpException, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import mongoose, { Model } from "mongoose";
 import { Post } from "src/shemas/post.schema";
 import { CreatePostDto } from "./dtos/createPost.dto";
 import { User } from "src/shemas/user.shema";
+import { UpdatePostDto } from "./dtos/update-post.dto";
 
 @Injectable()
 export class PostsService {
@@ -22,5 +23,32 @@ export class PostsService {
         await findUser.updateOne({$push : {posts: savePost._id}});
         
         return savePost;
+    }
+
+    async getPosts() {
+        return this.postModel.find();
+    }
+
+    async getPostById(id: string) {
+        const isValid = mongoose.Types.ObjectId.isValid(id);
+        if(!isValid) throw new HttpException('Invalid id', 400);
+        const post = await this.postModel.findById(id);
+        if(!post) throw new HttpException('Post not found', 404);
+        return post;
+    }
+
+    async updatePost(id: string, updatepostdto: UpdatePostDto) {
+        const isValid = mongoose.Types.ObjectId.isValid(id);
+        if(!isValid) throw new HttpException('Invalid id', 400);
+        const post = await this.postModel.findById(id);
+        if(!post) throw new HttpException('Post not found', 404);
+        post.updateOne(updatepostdto);
+        return post;
+    }
+
+    async deletePost(id: string) {
+        const isValid = mongoose.Types.ObjectId.isValid(id);
+        if(!isValid) throw new HttpException('Invalid id', 400);
+        return this.postModel.findByIdAndDelete(id);
     }
 }
